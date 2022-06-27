cc6695376400603fa0793c6dd6d4177f94e1c492
HKanwal <hsk532991@gmail.com>
2022-06-27 14:26:52 -0400

    Added click sound.

--- src/app/sub-btn/sub-btn.component.ts
+++ src/app/sub-btn/sub-btn.component.ts
@@ -6,5 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sub-btn.component.css']
 })
 export class SubBtnComponent {
+  audio: HTMLAudioElement | null = null;
 
+  ngOnInit() {
+    this.audio = document.querySelector("audio") || null;
+  }
+
+  playSound() {
+    if (this.audio)
+      this.audio.play();
+  }
 }
